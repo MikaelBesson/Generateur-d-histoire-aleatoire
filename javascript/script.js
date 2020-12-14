c26e31549e6f432b551b6b7590a45132a344b6d0
BessonMikael <besson.mikael04@gmail.com>
2020-12-14 16:43:41 +0100

    Generateur-d-histoire-aleatoire

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+let noms = ["pierre", "charlotte", "jean", "lucie", "charlie"];
+let objet = ["marteau","voiture","ballon","telephone","micro-onde"];
+let temps = ["-1000","15","25","5","7"];
+let lieux = ["fourmies","quinson","la lune","ailleurs","guadeloupe"];
+let verbes = ["manger","dormir","fumer","partager","courir"];
