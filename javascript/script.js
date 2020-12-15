b677158bb2d992f5bd4d6061456a097e52dd777f
BessonMikael <besson.mikael04@gmail.com>
2020-12-15 13:02:53 +0100

    Generateur-d-histoire-aleatoire

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,28 @@
-let noms = ["pierre", "charlotte", "jean", "lucie", "charlie"];
-let objet = ["marteau","voiture","ballon","telephone","micro-onde"];
-let temps = ["-1000","15","25","5","7"];
-let lieux = ["fourmies","quinson","la lune","ailleurs","guadeloupe"];
+let noms = ["Pierre aime", "Charlotte a envie de", "Jean n'aime pas", "Lucie veut bien", "Charlie s'en fout de"];
+let objet = ["avec un marteau","avec une voiture","avec un ballon","avec un telephone","avec un micro-onde"];
+let temps = ["quand il fait -1000","quand il fait 15°","quand il fait 25°","quand il fait 5°","quand il fait 7°"];
+let lieux = ["a fourmies","a quinson","sur la lune","a la plage"," en guadeloupe"];
 let verbes = ["manger","dormir","fumer","partager","courir"];
+let affiche = document.getElementById("texte");
+
+let name = document.getElementById("input");
+
+
+let boutonGenerer = document.getElementById("generate");
+
+boutonGenerer.addEventListener("click",function (){
+    let name1 = name.value;
+
+    let randomNoms = noms[Math.floor(Math.random() * noms.length)];
+
+    let randomobjet = objet[Math.floor(Math.random() * objet.length)];
+
+    let randomtemps = temps[Math.floor(Math.random() * temps.length)];
+
+    let randomlieux = lieux[Math.floor(Math.random() * lieux.length)];
+
+    let randomverbes = verbes[Math.floor(Math.random() * verbes.length)];
+    affiche.innerHTML = randomNoms+" "+randomverbes+"<br>"+randomobjet+" "+randomlieux+"<br>"+randomtemps+" avec "+name1;
+
+})
+
